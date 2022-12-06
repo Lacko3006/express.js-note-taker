92b99def357e41c161728931af4575c928949078
Sam Laxton <samlaxton@sams-air-2.home>
2022-12-06 17:44:55 +0000

    added db collect data get & fs.readfile

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const PORT = 3001;
+const path = require("path");
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// notes.html connected to local host
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// reads db to collect data
+let noteData
+fs.readFile("./db/db.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  noteData = data;
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// db connected to local host
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+
+app.listen(PORT, () => console.log(`${PORT} PORT is listening`));
